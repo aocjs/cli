ae34ee880855b05ea8bb7e4b742f074fffbffa12
Alessandro Bianco <code@alebianco.dev>
2021-11-10 15:00:24 +0100

    feat: cache input files
    be a good AOC citizen

--- src/lib/data/fetch.ts
+++ src/lib/data/fetch.ts
@@ -1,12 +1,32 @@
 import fetch from 'node-fetch'
+import { dirname, resolve } from 'path'
+import { mkdir, readFile, writeFile } from 'fs/promises'
+import { existsSync } from 'fs'
 
 export default async (): Promise<string[]> => {
-  const { year, session }: {year: string, session: string} = JSON.parse(
-    process.env.CONFIG ?? '{"year":2015}'
-  )
+  const { year }: {year: string} = JSON.parse(process.env.CONFIG ?? '{"year":2015}')
   const matchExp = (process.env.DAY ?? 'day1').match(/\d+/)
   const day: string = (matchExp !== null ? matchExp[0] : '1')
 
+  const text = await load(year, day)
+  return text.trim().split(/\r?\n/)
+}
+
+async function load (year: string, day: string): Promise<string> {
+  const filePath = resolve(process.cwd(), `./data/${year}/day${day}.txt`)
+
+  if (existsSync(filePath)) {
+    return await readFile(filePath, 'utf8')
+  } else {
+    const { session }: { session: string } = JSON.parse(process.env.CONFIG ?? '')
+    const content = await loadFromRemote(year, day, session)
+    await mkdir(dirname(filePath), { recursive: true })
+    await writeFile(filePath, content, 'utf8')
+    return content
+  }
+}
+
+async function loadFromRemote (year: string, day: string, session: string): Promise<string> {
   const response = await fetch(
     `https://adventofcode.com/${year}/day/${day}/input`,
     {
@@ -17,9 +37,7 @@ export default async (): Promise<string[]> => {
   )
 
   if (response.ok) {
-    const text: string = await response.text()
-
-    return text.trim().split(/\r?\n/)
+    return await response.text()
   }
   throw new Error(`Couldn't fetch data from https://adventofcode.com/${year}`)
 }
